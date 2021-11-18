781de30b50d77b9ade9e66b29ab2a6e321782da9
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2021-11-17 20:22:32 -0800

    Yes

---
+++ Snowman/Assets/GameController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public UnityEngine.UI.Text Message;
+    public UnityEngine.UI.Button StartButton;
+    public GameObject StartScreen;
+    public GameObject PlayScreen;
+    
+    public void StartGame()
+    {
+        this.Message.text = "Can you save the Snowman?";
+        this.StartButton.gameObject.SetActive(false);
+        this.StartScreen.SetActive(false);
+        this.PlayScreen.SetActive(true);
+    }
+    
+    public void OpenStartScreen()
+    {
+        this.StartScreen.SetActive(true);
+        this.PlayScreen.SetActive(false);
+    }
+
+    public void Start()
+    {
+        this.StartScreen.SetActive(true);
+        this.PlayScreen.SetActive(false);
+        
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
